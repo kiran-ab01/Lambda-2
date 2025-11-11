abc5cf4f56cef353cfb192bf001019bfef52f682
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-11-11 12:39:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket  = "my-terraform-state-bucket122"
-    key     = "lambda/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
 
 
 provider "aws" {
