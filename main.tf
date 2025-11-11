6da7c4e2eb9c1c397389e7676f1938b2339e2dec
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-11-11 12:31:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-state-bucket"
+    bucket  = "my-terraform-state-bucket122"
     key     = "lambda/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
