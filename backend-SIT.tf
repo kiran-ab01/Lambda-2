a12fecc9a027f7e11a26c6e41930a6ef150f9bad
kiran-ab01 <132429361+kiran-ab01@users.noreply.github.com>
2025-11-11 14:41:40 +0530

    Create backend-SIT.tf

---
+++ backend-SIT.tf
@@ -0,0 +1,9 @@
+# backend-uat.tf
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket122"
+    key    = "lambda/SIT/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
+}
